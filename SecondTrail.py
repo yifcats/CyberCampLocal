af383a476fb9953fedba23bb2d4abd71122d5788
yifcats <cats.yiftach@gmail.com>
2020-10-19 19:27:17 -0400

    Adding Class

--- SecondTrail.py
+++ SecondTrail.py
@@ -15,7 +15,6 @@ def which_interface():
 
         if intface in list(get_if_list()[:]):
             break
-
     return intface
 
 
@@ -23,17 +22,8 @@ def sniffing_tool():
     intface=which_interface()
     return sniff(iface=intface,prn=packet_handler)
 
-seq_num_syn=[]
-seq_num_acksyn=[]
-seq_num_ack=[]
 
-ack_num_syn=[]
-ack_num_acksyn=[]
-ack_num_ack=[]
 
-flag_syn=[]
-flag_acksyn=[]
-flag_ack=[]
 
 
 limit=10
@@ -42,7 +32,11 @@ def packet_handler(pkt):
     time=datetime.datetime.now()
     print_log(pkt,time)
 
-    syn_detection(pkt,time)
+    if pkt.haslayer(TCP):
+        F=pkt['TCP'].flags
+        s_com1,sa_com1,a_com1,r_com1,fpu_COM1,null_com1=pack_sorter_TCP(pkt,F)
+
+        syn_detection(s_com1,sa_com1,a_com1,time)
     # port_scan_detection(pkt)
 
 
@@ -79,7 +73,6 @@ def port_scan_detection(pkt):
     tcp_P = [x for x in range(0, 65535)]
 
 
-
     if TCP in pkt and pkt[TCP].dport in tcp_P:
         return True
     elif UDP in pkt and pkt[UDP].dport in udp_P:
@@ -90,64 +83,99 @@ def port_scan_detection(pkt):
         return False
 
 
-def syn_detection(pkt,time):
+def syn_detection(s_com,sa_com,a_com,time):
+    if len(s_com.seq_num) !=0 and len(sa_com.seq_num) !=0:
+        # print("seq: " + str(s_com.seq_num) + str(s_com.flag))
+        # print("ack: " + str(sa_com.ack_num) + str(sa_com.flag))
 
-    if pkt.haslayer(TCP):
+        New=[x+1 for x in s_com.seq_num]
+        New2=[x+1 for x in sa_com.seq_num]
 
-        F=pkt['TCP'].flags
-        SYN_COM=syn_find(pkt,F)
-        KSYN_COM=ack_syn_find(pkt,F)
-        K_COM=ack_find(pkt,F)
+        check1=set(New).issubset(sa_com.ack_num)
+        check2=set(New2).issubset(a_com.ack_num)
 
-        if len(SYN_COM[0]) !=0 and len(KSYN_COM[0]) !=0:
-
-            New=[x+1 for x in SYN_COM[0]]
-            New2=[x+1 for x in KSYN_COM[0]]
-
-            check1=set(New).issubset(KSYN_COM[1])
-            check2=set(New2).issubset(K_COM[1])
-
-            if check1:
-                print(str("[")+str(time) + str("]") + "\t"+ "Check 1 complete: communication secsessful")
-                if check2:
-                    print(str("[") + str(time) + str("]") + "\t"+ "Check 2 complete: No SYN Flooding")
-                else:
-                    if len(New) > len(K_COM[1]) + limit:
-                        logging.warning(str("[") + str(time) + str("]") + "\t"+ "Limit Exceeded, Check 2 Failed: SYN Flooding detected")
-                        print(str("[") + str(time) + str("]") + "\t"+ "Limit Exceeded, Check 2 Failed: SYN Flooding detected")
-                    # else:
-                        # logging.warning(str("[") + str(time) + str("]") + "\t"+ "Check 2 Failed: SYN overflow SYN > ACK")
+        if check1:
+            print(str("[")+str(time) + str("]") + "\t"+ "Check 1 complete: communication secsessful")
+            if check2:
+                print(str("[") + str(time) + str("]") + "\t"+ "Check 2 complete: No SYN Flooding")
             else:
-                logging.warning(str("[") + str(time) + str("]")+ "\t"+ "Communication Issues: SYN Not RECV")
+                if len(s_com.seq_num) > len(a_com.seq_num) + limit:
+                    logging.warning(str("[") + str(time) + str("]") + "\t"+ "Check 2 Failed: Limit Exceeded, SYN Flooding detected")
+                    print(str("[") + str(time) + str("]") + "\t"+ "Check 2 Failed: Limit Exceeded, SYN Flooding detected")
+                else:
+                    logging.warning(str("[") + str(time) + str("]") + "\t"+ "Check 2 Failed: SYN overflow SYN > ACK")
+                    print(str("[") + str(time) + str("]") + "\t"+ "Check 2 Failed: SYN overflow SYN > ACK")
+        else:
+            logging.warning(str("[") + str(time) + str("]")+ "\t"+ "Communication Issues: SYN Not RECV")
 
 
-def syn_find(pkt,F):
+
+
+class Sorting_Packets:
+
+    def __init__(self):
+        self.seq_num = []
+        self.ack_num = []
+        self.flag = []
+        self.src_port = []
+        self.des_port = []
+    def pack_data_TPC(self,pkt):
+        self.seq_num.append(pkt[TCP].seq)
+        self.ack_num.append(pkt[TCP].ack)
+        self.flag.append(pkt[TCP].flags)
+        self.src_port.append(pkt[TCP].sport)
+        self.des_port.append(pkt[TCP].dport)
+
+        return self.seq_num, self.ack_num, self.flag, self.src_port,self.des_port
+
+s_com=Sorting_Packets()
+sa_com=Sorting_Packets()
+a_com=Sorting_Packets()
+r_com=Sorting_Packets()
+fpu_com=Sorting_Packets()
+s_com=Sorting_Packets()
+null_com=Sorting_Packets()
+
+
+
+def pack_sorter_TCP(pkt,F):
     if F == 0x02 and F != 0x10:
-        seq_num_syn.append(pkt[TCP].seq)
-        ack_num_syn.append(pkt[TCP].ack)
-        flag_syn.append(pkt['TCP'].flags)
+        s_com.pack_data_TPC(pkt)
+        # print("seq: " + str(s_com.seq_num) + str(s_com.flag))
+        # print("ack: " + str(s_com.ack_num) + str(s_com.flag))
 
-    return seq_num_syn, ack_num_syn,flag_syn
+    elif F == 0x12:
+        sa_com.pack_data_TPC(pkt)
+        # print("seq: " + str(sa_com.seq_num) + str(sa_com.flag))
+        # print("ack: " + str(sa_com.ack_num) + str(sa_com.flag))
 
-def ack_syn_find(pkt,F):
-    if F == 0x12:
-        seq_num_acksyn.append(pkt[TCP].seq)
-        ack_num_acksyn.append(pkt[TCP].ack)
-        flag_acksyn.append(pkt['TCP'].flags)
+    elif F == 0x10:
+        a_com.pack_data_TPC(pkt)
+        # print("seq: " + str(a_com.seq_num) + str(a_com.flag))
+        # print("ack: " + str(a_com.ack_num) + str(a_com.flag))
+        # print(a_com.seq_num)
+    elif F == 0x4: # rst TPC_sS
+        r_com.pack_data_TPC(pkt)
+        # print(r_com.seq_num)
 
-    return seq_num_acksyn, ack_num_acksyn, flag_acksyn
-
-def ack_find(pkt,F):
-    if F == 0x10:
-        seq_num_ack.append(pkt[TCP].seq)
-        ack_num_ack.append(pkt[TCP].ack)
-        flag_ack.append(pkt['TCP'].flags)
-    return seq_num_ack, ack_num_ack, flag_ack
-
+    elif F == 0x29: # FPU TPC_sX
+        fpu_com.pack_data_TPC(pkt)
+    elif F == "":
+        null_com.pack_data_TPC(pkt)
 
 
+    return s_com,sa_com,a_com,r_com,fpu_com,null_com
+
+
+# s_com=[]
+# sa_com=[]
+# a_com=[]
+# r_com=[]
+# fpu_COM=[]
+# null_com=[]
 
 def main():
+
     logging.basicConfig(filename='sniff1.log', level=logging.INFO, filemode='w', format='%(levelname)s - %(message)s')
     packet_handler(sniffing_tool())
     return
--- port_attack_1.py
+++ port_attack_1.py
@@ -15,50 +15,88 @@ class Port_Scan:
     def __init__(self):
         self.tcpPorts = [ x for x in range(0, 65536) ]
 
-    def TPC_sS(self):
-        for self.port in self.tcpPorts:
-            packet = sr1(IP(dst=dst_ip)/TCP(sport=src_port,dport=self.port,flags="S"),timeout=0.1,verbose=0)
+    def scan_menu(self,scanner,ports):
+        if scanner == scan_list[0]:
+            Scan.TPC_sS(ports)
+        elif scanner == scan_list[1]:
+            Scan.TCP_sX(ports,"FPU")
+        elif scanner == scan_list[2]:
+            Scan.TCP_sX(ports,"")
+        elif scanner == scan_list[3]:
+            Scan.UDP_U(ports)
+
+    def TPC_sS(self,ports):
+        for self.port in ports:
+            packet = sr1(IP(dst=dst_ip)/TCP(sport=src_port,dport=self.port,flags="S"),timeout=0.05,verbose=0)
 
             if (packet.haslayer(TCP)) and (packet.getlayer(TCP).flags == 0x12):
-                send_rst = sr1(IP(dst=dst_ip)/TCP(sport=src_port,dport=self.port,flags="R"),timeout=0.1,verbose=0)
+                send_rst = sr1(IP(dst=dst_ip)/TCP(sport=src_port,dport=self.port,flags="R"),timeout=0.05,verbose=0)
                 print(str(self.port)+"/tpc \t Open")
 
 
+    def TCP_sX(self,ports,flaging):
 
-    def TPC_sX(self):
+        for self.port in ports:
 
-        for self.port in self.tcpPorts:
+            packet = sr1(IP(dst=dst_ip) / TCP(sport=src_port,dport=self.port, flags=flaging), timeout=0.001,verbose=0)
 
-            packet = sr1(IP(dst=dst_ip) / TCP(dport=self.port, flags="FPU"), timeout=10,verbose=0)
+            print(packet[TCP].flag=="<class 'NoneType'>")
 
-            if (str(type(packet)) == "<type 'NoneType'>"):
+            if (str(type(packet)) == "<class 'NoneType'>"):
                 print(str(self.port)+"/tpc \t Open|Filtered")
-            elif (packet.haslayer(TCP)) and (packet.getlayer(TCP).flags == 0x14):
-                print(str(self.port) + "/tpc \t Closed")
+
             elif (packet.haslayer(ICMP)):
                 if (int(packet.getlayer(ICMP).type) == 3 and int(packet.getlayer(ICMP).code) in [1, 2, 3, 9, 10, 13]):
                     print(str(self.port) + "/tpc \t Filtered")
 
+    def UDP_U(self, ports):
+
+
+        for self.port in ports:
+
+            packet = sr1(IP(dst=dst_ip) / UDP(sport=src_port,dport=self.port),timeout=10,verbose=0)
+
+            # print(str(type(packet)))
+
+            if (str(type(packet)) == "<class 'NoneType'>"):
+                print(str(self.port) + "/UDP \t Open|Filtered")
+
+            elif packet.haslayer(UDP):
+                print(str(self.port) + "/UDP \t Open")
+
+            elif (int(packet.getlayer(ICMP).type) == 3 and int(packet.getlayer(ICMP).code) in [1, 2, 9, 10, 13]):
+                print(str(self.port) + "/UDP \t Filtered")
+
+            # else:
+            #     print(str(self.port) + "/UDP \t Closed")
 
 def UserInterp():
-    scan_list=("TPC_sS","TPC_sX")
     while True:
         print("Enter a valid Scan: " + str(scan_list))
         scan_choice=input("Enter the interface: ")
 
         if scan_choice in list(scan_list):
+            while True:
+                min_P=int(input("Enter Valid min port number: "))
+                max_P=int(input("Enter Valid max port number: "))
+
+                if min_P>0 and min_P<max_P and min_P<65536:
+                    break
             break
 
-    return scan_choice
+    return scan_choice,[ x for x in range(min_P, max_P) ]
 
 Scan=Port_Scan()
+scan_list = ("TPC_sS", "TCP_sX", 'TPC_null','UDP_U')
+
+
+
+
 
 def main():
-    SC=UserInterp()
-    if SC == 'TPC_sS':
-        Scan.TPC_sS()
-    elif SC == 'TPC_sX':
-        Scan.TPC_sX()
+    SC,ports=UserInterp()
+    Scan.scan_menu(SC,ports)
+
 
 
 
